d31416f24e3e67abdfaa2852354ae9d644dc3984
Brett DiFrischia <orderthruchaos@gmail.com>
2014-08-20 09:17:56 -0500

    Adding tests and template from original repository.

--- test/path_utils_test.exs
+++ test/path_utils_test.exs
@@ -1,7 +1,149 @@
+Code.require_file "test_helper.exs", __DIR__
+
 defmodule PathUtilsTest do
-  use ExUnit.Case
+  use PathUtils.Case
+  pu_alias
+
+
+  @win32_dir   Path.join(@data_dir, "win32")
+  @posix_dir   Path.join(@data_dir, "posix")
+  @win32_file  Path.join(@win32_dir, "some_file.txt")
+  @posix_file  Path.join(@posix_dir, "some_file.txt")
+
 
   test "the truth" do
-    assert 1 + 1 == 2
+    assert true
   end
+
+  # test "Test the file properties of '#{@test_path}'" do
+  #   # IO.puts ""
+  #   # IO.puts @test_file
+  #   # IO.puts @test_path
+  #   # IO.puts ""
+  #   assert File.exists?(@test_path)
+  #   assert File.regular?(@test_path)
+  # end
+
+  # test "Test the properties of the directory '#{@test_dir}'" do
+  #   assert File.exists?(@test_dir)
+  #   assert File.dir?(@test_dir)
+  # end
+
+  # test "Directory is not a symlink." do
+  #   assert ! PU.symlink?(@test_dir)
+  # end
+
+  # test "File is not a symlink." do
+  #   assert ! PU.symlink?(@test_path)
+  # end
+
+  # test "Basic test path properties" do
+  #   assert File.dir?     @data_dir
+  #   assert File.dir?     @win32_dir
+  #   assert File.dir?     @posix_dir
+  #   assert File.regular? @win32_file
+  #   assert File.regular? @posix_file
+  # end
+
+  # test "directory:  directories" do
+  #   assert dc(@test_dir) == dc(PU.directory(@test_dir))
+  #   assert dc(@data_dir) == dc(PU.directory(@data_dir))
+
+  #   rel_data = Path.relative_to_cwd(@data_dir)
+  #   assert rel_data != @data_dir
+  #   assert dc(@data_dir) == dc(PU.directory(rel_data))
+  # end
+
+  # test "directory:  files" do
+  #   assert @posix_dir == PU.directory(@posix_file)
+
+  #   rel_posix = Path.relative_to_cwd(@posix_file)
+  #   assert rel_posix != @posix_file
+  #   assert @posix_dir == PU.directory(rel_posix)
+  # end
+
+  # test "dcd:  no brainers" do
+  #   check_dcd_of(@test_dir, @test_dir, @test_dir)
+  #   check_dcd_of(@data_dir, @data_dir, @data_dir)
+  # end
+
+  # test "dcd:  no brainers, relative dirs" do
+  #   check_dcd_of_rel(@data_dir, @data_dir, @data_dir)
+  # end
+
+  # test "dcd:  directory portion of paths" do
+  #   check_dcd_of(@posix_dir, @posix_file, @posix_dir)
+  # end
+
+  # test "dcd:  directory portion of relative paths" do
+  #   check_dcd_of_rel(@posix_dir, @posix_file, @posix_dir)
+  # end
+
+  # test "dcd:  Differences occur earlier (1)" do
+  #   check_dcd_of(@data_dir, @posix_dir, @win32_dir)
+  #   check_dcd_of(@data_dir, @posix_file, @win32_dir)
+  #   check_dcd_of(@data_dir, @posix_dir, @win32_file)
+  #   check_dcd_of_rel(@data_dir, @posix_dir, @win32_dir)
+  #   check_dcd_of_rel(@data_dir, @posix_file, @win32_dir)
+  #   check_dcd_of_rel(@data_dir, @posix_dir, @win32_file)
+  # end
+
+  # test "dcd:  Differences occur earlier (2)" do
+  #   check_dcd_of(@test_dir, @test_file, @lib_file)
+  #   check_dcd_of(@test_dir, @posix_dir, @lib_file)
+  # end
+
+  # @tag :no_symlinks
+  # test "maxsymlinks output (no symlinks)" do
+  #   assert 0 == PU.maxsymlinks()
+  # end
+
+  # @tag :has_symlinks
+  # test "maxsymlinks output (has symlinks)" do
+  #   expected = case :os.type do
+  #     { :win32, _ } -> 31
+  #     _             -> 64
+  #   end
+
+  #   assert expected == PU.maxsymlinks()
+  # end
+
+
+  # # Helpers
+
+
+  # defp check_dcd_of(exp_dir, p1, p2) do
+  #   assert dc(exp_dir) == dc(PU.dcd(p1, p2))
+  #   assert dc(exp_dir) == dc(PU.dcd(p2, p1))
+  # end
+
+
+  # defp check_dcd_of_rel(exp_dir, p1, p2) do
+  #   r1 = Path.relative_to_cwd(p1)
+  #   r2 = Path.relative_to_cwd(p2)
+
+  #   # Check inputs
+  #   assert r1 != p1
+  #   assert r2 != p2
+  #   if p1 == p2 do
+  #     assert r1 == r2
+  #   else
+  #     assert r1 != r2
+  #   end
+
+  #   assert dc(exp_dir) == dc(PU.dcd(r1, p2))
+  #   assert dc(exp_dir) == dc(PU.dcd(p2, r1))
+  #   assert dc(exp_dir) == dc(PU.dcd(p1, r2))
+  #   assert dc(exp_dir) == dc(PU.dcd(r2, p1))
+  #   assert dc(exp_dir) == dc(PU.dcd(r1, r2))
+  #   assert dc(exp_dir) == dc(PU.dcd(r2, r1))
+  # end
+
+  # defp dc(s) do
+  #   case :os.type do
+  #     { :win32, _ } -> String.downcase(s)
+  #     _             -> s
+  #   end
+  # end
+
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,114 @@
+has_symlinks = case :file.read_link(Path.expand(__ENV__.file)) do
+  {:error, :enotsup} -> false
+  _                  -> true
+end
+
+if has_symlinks do
+  incl_symlinks_str = "has_symlinks"
+  tags_to_exclude = [:todo, :os, :os_sym, :no_symlinks]
+else
+  incl_symlinks_str = "no_symlinks"
+  tags_to_exclude = [:todo, :os, :os_sym, :has_symlinks]
+end
+
+# incl_symlinks_str = case :file.read_link(Path.expand(__ENV__.file)) do
+#   {:error, :enotsup} -> "no_symlinks"
+#   _                  -> "has_symlinks"
+# end
+# case :file.read_link(Path.expand(__ENV__.file)) do
+#   {:error, :enotsup} -> tags_to_exclude = [:os, :os_sym, :has_symlinks]
+#   _                  -> tags_to_exclude = [:os, :os_sym, :no_symlinks]
+# end
+
+{ this_os, _ } = :os.type
+os_sym_tag     = :"#{this_os}_#{incl_symlinks_str}"
+
+
+ExUnit.configure(
+  exclude: tags_to_exclude,
+  include: [os: this_os, os_sym: os_sym_tag] )
+
 ExUnit.start()
+
+
+defmodule PathUtils.Case do
+  use ExUnit.CaseTemplate
+  @moduledoc false
+
+
+  using do
+    { os_id, _ } = :os.type
+    os_str = case os_id do
+      :unix -> "posix"
+      _     -> to_string os_id
+    end
+    d = __DIR__
+
+    quote do
+      import  unquote(__MODULE__)
+      require PathUtils
+
+      @test_os     unquote(os_id)
+      @test_os_str unquote(os_str)
+      @test_dir    System.cwd
+      @test_file   Path.relative_to_cwd(__ENV__.file)
+      @test_path   Path.expand(@test_file)
+      @data_dir    Path.expand(Path.join(unquote(d), "data"))
+      @lib_file    Path.join(["test", "..", "lib", "canon_path.ex"])
+      @orig_file   "some_file.txt"
+      @link_trgt   Path.join([unquote(d), "data", @test_os_str, @orig_file])
+      @rel_link    Path.join([unquote(d), "data", @test_os_str, "rel_link.txt"])
+
+    end
+  end
+
+  defmacro pu_alias do
+    quote do
+      alias   PathUtils, as: PU
+    end
+  end
+
+  defmacro refute_cwd_contains_symlink do
+    quote do
+      symlinks = symlinks_in_path(System.cwd) |>
+        Enum.map &("Symlink:     #{&1}")
+      msg = [
+        "System.cwd must not contain symlinks:",
+        "System.cwd:  #{System.cwd}" | symlinks
+        ] |> Enum.join "\n        "
+
+      # msg = "System.cwd must not contain symlinks: #{System.cwd}"
+      # refute cwd_contains_symlink?(), "System.cwd must not contain symlinks: #{System.cwd}"
+      refute cwd_contains_symlink?(), msg
+    end
+  end
+
+  def cwd_contains_symlink?, do: path_contains_symlink?(System.cwd)
+
+  def path_contains_symlink?(path) do
+    Enum.any? cumulative_dirs(path), &is_symlink?/1
+  end
+
+  def symlinks_in_path(p) do
+    Enum.filter cumulative_dirs(p), &is_symlink?/1
+  end
+
+
+  # Helpers
+
+
+  defp is_symlink?(p) do
+    case :file.read_link(p) do
+      {:ok, _} -> true
+      _        -> false
+    end
+  end
+
+  defp cumulative_dirs(d) do
+    f = fn
+      x, []          -> [x]
+      x, [h|_] = acc -> [ Path.join(h, x) | acc ]
+    end
+    Path.split(d) |> List.foldl([], f) |> Enum.reverse
+  end
+end
