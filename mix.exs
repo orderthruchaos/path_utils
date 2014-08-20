118080a1550364c4c40f461892d9092b6cba18fe
Brett DiFrischia <orderthruchaos@gmail.com>
2014-08-20 09:07:38 -0500

    Updating Mix file.

--- mix.exs
+++ mix.exs
@@ -1,18 +1,35 @@
 defmodule PathUtils.Mixfile do
   use Mix.Project
 
+  @version "0.0.1"
+  @github  "https://github.com/orderthruchaos/path_utils"
+
   def project do
-    [app: :path_utils,
-     version: "0.0.1",
-     elixir: "~> 0.15.1",
-     deps: deps]
+    [
+      app: :path_utils,
+      version: @version,
+      elixir: ">= 0.15.0",
+      deps: deps,
+      description: "Path utilities for Elixir.",
+      package: [
+        contributors: ["Brett DiFrischia"],
+        licenses: ["Apache 2.0"],
+        links: %{ "Github" => @github },
+      ],
+      docs: [
+        readme: true,
+        main: "README",
+        source_url: @github,
+        source_ref: @version,
+      ],
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :os_utils]]
   end
 
   # Dependencies can be Hex packages:
@@ -25,6 +42,9 @@ defmodule PathUtils.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:os_utils, "~> 0.1"},
+      {:ex_doc, "~> 0.5", only: :dev},
+    ]
   end
 end
