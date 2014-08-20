275fc7c48dd5225792fb54085645713c99ceb69e
Brett DiFrischia <orderthruchaos@gmail.com>
2014-08-20 09:31:22 -0500

    Adding commented out test files.

---
+++ test/path_utils_todo_test.exs
@@ -0,0 +1,18 @@
+# Code.require_file "test_helper.exs", __DIR__
+# 
+# defmodule PathUtilsTodoTest do
+#   use CanonPath.Case
+# 
+# 
+#   @moduletag :todo
+# 
+# 
+#   test "flunk should not happen!", do: flunk "TODO!!!"
+# 
+# 
+#   # test "PU placeholder" do
+#   #   assert 64 == PU.maxsymlinks()
+#   # end
+# 
+# 
+# end
---
+++ test/path_utils_unix_test.exs
@@ -0,0 +1,58 @@
+# Code.require_file "test_helper.exs", __DIR__
+# 
+# defmodule CanonpathPathUtilsUnixTest do
+#   use CanonPath.Case
+#   pu_alias
+# 
+# 
+#   @tag os: :unix
+#   test "Test the file properties of '#{@test_path}'" do
+#     # IO.puts ""
+#     # IO.puts @test_file
+#     # IO.puts @test_path
+#     # IO.puts ""
+#     assert File.exists?(@test_path)
+#     assert File.regular?(@test_path)
+#   end
+# 
+#   @tag os_sym: :unix_has_symlinks
+#   test "Test the file properties of '#{@rel_link}'" do
+#     # IO.puts ""
+#     # IO.puts @rel_link
+#     # IO.puts ""
+#     assert File.exists?(@rel_link)
+#     assert @link_trgt != @rel_link
+#     check_link @orig_file, @rel_link
+#   end
+# 
+#   @tag os_sym: :unix_has_symlinks
+#   test "symlink? returns true for #{@rel_link}" do
+#     assert PU.symlink?(@rel_link)
+#   end
+# 
+#   @tag os_sym: :unix_no_symlinks
+#   test "symlink? returns false for #{@rel_link} (not supported)" do
+#     refute PU.symlink?(@rel_link)
+#   end
+# 
+#   @tag os_sym: :unix_has_symlinks
+#   test "maxsymlinks output" do
+#     assert 64 == PU.maxsymlinks()
+#   end
+# 
+# 
+#   # Helpers
+# 
+# 
+#   defp check_link(target, link) do
+#     process_read_link(target, link, :file.read_link(@rel_link))
+#   end
+# 
+#   defp process_read_link(target, _link, {:ok, path}) do
+#     assert target == to_string(path)
+#   end
+# 
+#   defp process_read_link(target, link, _) do
+#     flunk "Could not read symlink: #{link} -> #{target}"
+#   end
+# end
