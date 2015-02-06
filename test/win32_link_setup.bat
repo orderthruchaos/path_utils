9ff31068eef6b2fa0e526dea398c7fd5c1a6881e
Brett N. DiFrischia <orderthruchaos@gmail.com>
2015-02-06 15:34:44 -0600

    More symlinks for testing on Windows.

--- test/win32_link_setup.bat
+++ test/win32_link_setup.bat
@@ -172,6 +172,166 @@ IF ERRORLEVEL 1 (
   ECHO ERROR: mklink failed.  This script must be run as an administrator.
   GOTO :go_back_to_orig_dir
 )
+MKLINK /D d33 d32
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d34 d33
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d35 d34
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d36 d35
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d37 d36
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d38 d37
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d39 d38
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d40 d39
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d41 d40
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d42 d41
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d43 d42
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d44 d43
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d45 d44
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d46 d45
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d47 d46
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d48 d47
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d49 d48
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d50 d49
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d51 d50
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d52 d51
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d53 d52
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d54 d53
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d55 d54
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d56 d55
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d57 d56
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d58 d57
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d59 d58
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d60 d59
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d61 d60
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d62 d61
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d63 d62
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
+MKLINK /D d64 d63
+IF ERRORLEVEL 1 (
+  ECHO ERROR: mklink failed.  This script must be run as an administrator.
+  GOTO :go_back_to_orig_dir
+)
 
 
 :go_back_to_orig_dir
